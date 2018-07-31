6ed59293c4cca224d48c9f5008dc48242494fb9f
hngel <41763813+hngel@users.noreply.github.com>
2018-07-31 20:41:49 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -287,7 +287,7 @@ client.on('message', message => {
 var prefix = "==";
 if(message.content.startsWith('==bc')) {
 if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
-if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
+if(!message.member.hasPermission('ADMINISTRATOR','269031102340005888')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
 let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
 let copy = "faisal";
 let request = `Requested By ${message.author.username}`;
